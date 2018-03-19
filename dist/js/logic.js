d63d26880215e97a098c574ba105143c4064faf8
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-19 18:25:22 +0000

    add click detection on original canvas

---
+++ dist/js/logic.js
@@ -0,0 +1,51 @@
+"use strict";
+
+if (!Detector.webgl) Detector.addGetWebGLMessage();
+
+// global variables for this scripts
+var OriginalImg = void 0,
+    SegmentImg = void 0;
+
+var mouse = new THREE.Vector2();
+var raycaster = new THREE.Raycaster();
+var mousePressed = false;
+
+init();
+animate();
+
+// initilize the page
+function init() {
+    var filename = "models/nrrd/columna01.nrrd"; // change your nrrd file
+    var idDiv = 'original';
+    OriginalImg = new InitCanvas(idDiv, filename);
+    OriginalImg.init();
+    console.log(OriginalImg);
+
+    filename = "models/nrrd/columnasegmentado01.nrrd"; // change your nrrd file
+    idDiv = 'segment';
+    SegmentImg = new InitCanvas(idDiv, filename);
+    SegmentImg.init();
+}
+
+document.addEventListener('mousedown', onDocumentMouseDown, false);
+document.addEventListener('mouseup', onDocumentMouseUp, false);
+
+function onDocumentMouseDown(event) {
+    mousePressed = true;
+    mouse.x = event.clientX / window.innerWidth * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    raycaster.setFromCamera(mouse.clone(), OriginalImg.camera);
+    var objects = raycaster.intersectObjects(OriginalImg.scene.children);
+    console.log(objects);
+}
+function onDocumentMouseUp(event) {
+    mousePressed = false;
+}
+
+function animate() {
+
+    requestAnimationFrame(animate);
+    OriginalImg.animate();
+    SegmentImg.animate();
+}
+//# sourceMappingURL=logic.js.map
--- js/logic.js
+++ js/logic.js
@@ -4,6 +4,11 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 let OriginalImg, 
     SegmentImg;
 
+var mouse = new THREE.Vector2();
+var raycaster = new THREE.Raycaster();
+var mousePressed = false;
+
+
 
 init();
 animate();
@@ -15,7 +20,8 @@ function init ()
     let filename = "models/nrrd/columna01.nrrd"; // change your nrrd file
     let idDiv = 'original';
     OriginalImg = new InitCanvas(idDiv, filename );
-    OriginalImg.init(); 
+    OriginalImg.init();
+    console.log(OriginalImg);
     
     filename = "models/nrrd/columnasegmentado01.nrrd"; // change your nrrd file
     idDiv = 'segment';
@@ -23,7 +29,26 @@ function init ()
     SegmentImg.init(); 
 }
 
+document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+
+function onDocumentMouseDown( event ) {
+    mousePressed = true;
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    raycaster.setFromCamera( mouse.clone(), OriginalImg.camera );
+    var objects = raycaster.intersectObjects(OriginalImg.scene.children);
+    console.log(objects);
+}
+function onDocumentMouseUp( event ) { mousePressed = false}
+
+
+
+
+
 function animate() {
+
+
     requestAnimationFrame( animate );
     OriginalImg.animate();
     SegmentImg.animate();
